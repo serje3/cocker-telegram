a1cc2dd2ac3e636956da2ae1bdf4b9888a082775
serje3 <serjerin322@gmail.com>
2024-08-22 01:05:28 +0300

    fix instructions

--- db/hooks/custom_instructions.py
+++ db/hooks/custom_instructions.py
@@ -4,9 +4,9 @@ custom_instructions_collection = db['custom_instructions']
 
 
 async def insert_instruction(chat_id: int, instruction: str):
-    await custom_instructions_collection.find_one_and_update({'chat_id': chat_id},
-                                                             {'chat_id': chat_id, 'instruction': instruction},
-                                                             upsert=True)
+    await custom_instructions_collection.find_one_and_replace({'chat_id': chat_id},
+                                                              {'chat_id': chat_id, 'instruction': instruction},
+                                                              upsert=True)
 
 
 async def find_instruction(chat_id: int) -> str | None:
